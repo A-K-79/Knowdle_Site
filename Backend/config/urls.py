0d9d08dfc63f1e97b4042a42f4bcef066816ae1e
Abhijith342 <987abhijith654@gmail.com>
2026-07-13 21:26:01 +0530

    Fix static media files URL pattern

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -52,5 +52,4 @@ urlpatterns = [
 ]
 
 # MEDIA FILES
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
